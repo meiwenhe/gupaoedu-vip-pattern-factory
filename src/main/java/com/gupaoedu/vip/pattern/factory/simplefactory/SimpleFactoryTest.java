ad55912ab26dcdd726233c85eb5b3c4d16063ece
meiwenhe <meiwenhe123@163.com>
2019-04-10 00:52:25 +0800

    修改aa1

--- src/main/java/com/gupaoedu/vip/pattern/factory/simplefactory/SimpleFactoryTest.java
+++ src/main/java/com/gupaoedu/vip/pattern/factory/simplefactory/SimpleFactoryTest.java
@@ -23,7 +23,7 @@ public class SimpleFactoryTest {
         ICourse course = factory.create(JavaCourse.class);
         ICourse pythonCourse = factory.create(PythonCourse.class);
         course.record();
-        System.out.println("aaa");
+        System.out.println("---------");
         pythonCourse.record();
 
     }
